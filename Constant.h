35b9e9d037eea11e768b5d9e22905c67e94603a4
Chakyeoungho <86039781+Chakyeoungho@users.noreply.github.com>
2022-01-07 20:11:48 +0900

    초기 커밋.

---
+++ Constant.h
@@ -0,0 +1,80 @@
+﻿#ifndef __CONSTANT_H__
+#define __CONSTANT_H__
+
+/*
+* 보드판 상태
+* 0 : 아무것도 없음 (타일 닫힘)
+* 10 : 아무것도 없음 (타일 열림)
+* 1 ~ 8 : 주변 지뢰의 개수 (타일 닫힘)
+* 11 ~ 18 : 주변 지뢰의 개수 (타일 열림)
+* 20 : 지뢰
+* 21 : 깃발
+* 22 : 물음표
+*/
+enum {
+	nothing_closed,
+	mine_num1_closed,
+	mine_num2_closed,
+	mine_num3_closed,
+	mine_num4_closed,
+	mine_num5_closed,
+	mine_num6_closed,
+	mine_num7_closed,
+	mine_num8_closed,
+	nothing_open = 10,
+	mine_num1_open,
+	mine_num2_open,
+	mine_num3_open,
+	mine_num4_open,
+	mine_num5_open,
+	mine_num6_open,
+	mine_num7_open,
+	mine_num8_open,
+	mine = 20,
+	flag,
+	questionMark
+};
+
+// 게임 난이도
+#define EASY   1000    // 난이도 쉬움
+#define NORMAL 1001    // 난이도 보통
+#define HARD   1002    // 난이도 어려움
+
+// 보드판 x, y축 크기
+#define EASY_X_COUNT    9    // 쉬움 난이도 x축 개수
+#define EASY_Y_COUNT    9    // 쉬움 난이도 y축 개수
+#define NORMAL_X_COUNT 16    // 보통 난이도 x축 개수
+#define NORMAL_Y_COUNT 16    // 보통 난이도 y축 개수
+#define HARD_X_COUNT   30    // 어려움 난이도 x축 개수
+#define HARD_Y_COUNT   16    // 어려움 난이도 y축 개수
+
+// 타일 하나의 크기
+#define EASY_GRID_SIZE   40    // 쉬움 난이도 사각형 크기
+#define NORMAL_GRID_SIZE 30    // 보통 난이도 사각형 크기
+#define HARD_GRID_SIZE   25    // 어려움 난이도 사각형 크기
+
+// 총 타일 개수
+#define EASY_BOARD_NUM   (( 9) * ( 9))    // 쉬움 난이도 총 타일 개수
+#define NORMAL_BOARD_NUM ((16) * (16))    // 보통 난이도 총 타일 개수
+#define HARD_BOARD_NUM   ((30) * (16))    // 어려움 난이도 총 타일 개수
+
+// 지뢰의 총 개수
+#define EASY_MINE_NUM   10    // 쉬움 난이도 총 지뢰 개수
+#define NORMAL_MINE_NUM 40    // 보통 난이도 총 지뢰 개수
+#define HARD_MINE_NUM   99    // 어려움 난이도 총 지뢰 개수
+
+// 마우스 클릭 상태
+#define LBUTTON 100    // 왼쪽 클릭
+#define RBUTTON 101    // 오른쪽 클릭
+
+// 자주 사용하는 색상
+#define BLACK  RGB(0, 0, 0)          // 검은색
+#define WHITE  RGB(255, 255, 255)    // 흰색
+#define ORANGE RGB(200, 150, 30)     // 주황색
+
+// 현재 개임 스텝(난이도 선택화면, 게임 플레이 화면)
+#define STARTSCR 0    // 게임 시작 화면
+#define SELECTLV 1    // 난이도 선택 화면
+#define PLAYGAME 2    // 게임중 화면
+
+#endif // !__CONSTANT_H__
---
+++ Minesweeper.cpp
@@ -0,0 +1,226 @@
+﻿#include "pch.h"
+#include "tipsware.h"
+#include "Constant.h"
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct _GameData // 게임 플레이중 필요한 데이터
+{
+	char state_hard[HARD_Y_COUNT][HARD_X_COUNT];    // 가장 큰 사이즈의 보드만 있어도 모든 난이도의 게임을 만들 수 있다
+	char click;
+	char tile_state;    // 타일 상태
+	int level;     // 선택한 난이도
+	int game_step;    // 현재 게임 단계
+} GameData, *pGameData;
+
+void selectLvButton();    // 난이도 선택 버튼 생성
+void easyLv_board(pGameData ap_data);    // 쉬움 난이도 판 생성
+void normalLv_board(pGameData ap_data);    // 보통 난이도 판 생성
+void hardLv_board(pGameData ap_data);    // 어려움 난이도 판 생성
+
+void OnLButtonDown(int a_mixed_key, POINT a_pos)
+{
+	pGameData p_data = (pGameData)GetAppData();
+
+	if (a_mixed_key & MK_CONTROL) {
+		if (p_data->game_step == 1) {
+			if (p_data->level == EASY) {
+				unsigned int x = (unsigned int)a_pos.x / EASY_GRID_SIZE, y = (unsigned int)a_pos.y / EASY_GRID_SIZE;
+				if (x < EASY_X_COUNT && y < EASY_Y_COUNT) {
+					if (p_data->state_hard[y][x] == nothing_closed)
+						p_data->state_hard[y][x] = flag;
+					else if (p_data->state_hard[y][x] == flag)
+						p_data->state_hard[y][x] = questionMark;
+					else if (p_data->state_hard[y][x] == questionMark)
+						p_data->state_hard[y][x] = nothing_closed;
+
+					easyLv_board(p_data);
+				}
+			}
+			else if (p_data->level == NORMAL) {
+				unsigned int x = (unsigned int)a_pos.x / NORMAL_GRID_SIZE, y = (unsigned int)a_pos.y / NORMAL_GRID_SIZE;
+				if (x < NORMAL_X_COUNT && y < NORMAL_Y_COUNT) {
+					if (p_data->state_hard[y][x] == nothing_closed)
+						p_data->state_hard[y][x] = flag;
+					else if (p_data->state_hard[y][x] == flag)
+						p_data->state_hard[y][x] = questionMark;
+					else if (p_data->state_hard[y][x] == questionMark)
+						p_data->state_hard[y][x] = nothing_closed;
+
+					normalLv_board(p_data);
+				}
+			}
+			else if (p_data->level == HARD) {
+				unsigned int x = (unsigned int)a_pos.x / HARD_GRID_SIZE, y = (unsigned int)a_pos.y / HARD_GRID_SIZE;
+				if (x < HARD_X_COUNT && y < HARD_Y_COUNT) {
+					if (p_data->state_hard[y][x] == nothing_closed)
+						p_data->state_hard[y][x] = flag;
+					else if (p_data->state_hard[y][x] == flag)
+						p_data->state_hard[y][x] = questionMark;
+					else if (p_data->state_hard[y][x] == questionMark)
+						p_data->state_hard[y][x] = nothing_closed;
+
+					hardLv_board(p_data);
+				}
+			}
+		}
+	}
+	else {
+		if (p_data->game_step == 0) {
+			if (a_pos.x >= 20 && a_pos.x <= 108 && a_pos.y >= 10 && a_pos.y <= 38) {
+				easyLv_board(p_data);
+				p_data->level = EASY;
+				p_data->game_step++;
+			}
+			else if (a_pos.x >= 120 && a_pos.x <= 208 && a_pos.y >= 10 && a_pos.y <= 38) {
+				normalLv_board(p_data);
+				p_data->level = NORMAL;
+				p_data->game_step++;
+			}
+			else if (a_pos.x >= 220 && a_pos.x <= 308 && a_pos.y >= 10 && a_pos.y <= 38) {
+				hardLv_board(p_data);
+				p_data->level = HARD;
+				p_data->game_step++;
+			}
+		}
+		// 지뢰 타일 오픈
+		else if (p_data->game_step == 1) {
+			if (p_data->level == EASY) {
+				unsigned int x = (unsigned int)a_pos.x / EASY_GRID_SIZE, y = (unsigned int)a_pos.y / EASY_GRID_SIZE;
+				if (x < EASY_X_COUNT && y < EASY_Y_COUNT) {
+					if (p_data->state_hard[y][x] == nothing_closed)
+						p_data->state_hard[y][x] = nothing_open;
+
+					easyLv_board(p_data);
+				}
+			}
+			else if (p_data->level == NORMAL) {
+				unsigned int x = (unsigned int)a_pos.x / NORMAL_GRID_SIZE, y = (unsigned int)a_pos.y / NORMAL_GRID_SIZE;
+				if (x < NORMAL_X_COUNT && y < NORMAL_Y_COUNT) {
+					if (p_data->state_hard[y][x] == nothing_closed)
+						p_data->state_hard[y][x] = nothing_open;
+
+					normalLv_board(p_data);
+				}
+			}
+			else if (p_data->level == HARD) {
+				unsigned int x = (unsigned int)a_pos.x / HARD_GRID_SIZE, y = (unsigned int)a_pos.y / HARD_GRID_SIZE;
+				if (x < HARD_X_COUNT && y < HARD_Y_COUNT) {
+					if (p_data->state_hard[y][x] == nothing_closed)
+						p_data->state_hard[y][x] = nothing_open;
+
+					hardLv_board(p_data);
+				}
+			}
+		}
+	}
+
+
+}
+
+MOUSE_MESSAGE(OnLButtonDown, NULL, NULL)
+
+int main()
+{
+	GameData data;
+	memset(&data, 0, sizeof(GameData));
+	SetAppData(&data, sizeof(GameData));
+
+	SelectFontObject("굴림", 20, 1);
+	selectLvButton();
+
+	ShowDisplay();
+	return 0;
+}
+
+/*
+* 보드판 상태
+* 0 : 아무것도 없음 (타일 닫힘)
+* 10 : 아무것도 없음 (타일 열림)
+* 1 ~ 8 : 주변 지뢰의 개수 (타일 닫힘)
+* 11 ~ 18 : 주변 지뢰의 개수 (타일 열림)
+* 20 : 지뢰
+* 21 : 깃발
+* 22 : 물음표
+*/
+
+void selectLvButton()
+{
+	Rectangle(20, 10, 108, 38, ORANGE, ORANGE);    // 쉬움lv
+	TextOut(43, 13, BLACK, "쉬움");
+
+	Rectangle(120, 10, 208, 38, ORANGE, ORANGE);    // 보통lv
+	TextOut(143, 13, BLACK, "보통");
+
+	Rectangle(220, 10, 308, 38, ORANGE, ORANGE);    // 어려움lv
+	TextOut(233, 13, BLACK, "어려움");
+}
+
+void easyLv_board(pGameData ap_data)
+{
+	Clear();
+	for (int y = 0; y < EASY_Y_COUNT; y++) {
+		for (int x = 0; x < EASY_X_COUNT; x++) {
+			Rectangle(x * EASY_GRID_SIZE, y * EASY_GRID_SIZE, (x + 1) * EASY_GRID_SIZE, (y + 1) * EASY_GRID_SIZE, RGB(0, 100, 200), RGB(0, 0, 128));
+		}
+	}
+
+	for (int y = 0; y < EASY_Y_COUNT; y++) {
+		for (int x = 0; x < EASY_X_COUNT; x++) {
+			if (ap_data->state_hard[y][x] == nothing_open)
+				Rectangle(x * EASY_GRID_SIZE, y * EASY_GRID_SIZE, (x + 1) * EASY_GRID_SIZE, (y + 1) * EASY_GRID_SIZE, RGB(200, 100, 0), RGB(128, 0, 0));
+			else if (ap_data->state_hard[y][x] == flag)
+				Ellipse(x * EASY_GRID_SIZE, y * EASY_GRID_SIZE, (x + 1) * EASY_GRID_SIZE, (y + 1) * EASY_GRID_SIZE, RGB(200, 100, 0), RGB(128, 0, 0));
+			else if (ap_data->state_hard[y][x] == questionMark)
+				Ellipse(x * EASY_GRID_SIZE, y * EASY_GRID_SIZE, (x + 1) * EASY_GRID_SIZE, (y + 1) * EASY_GRID_SIZE, WHITE, BLACK);
+		}
+	}
+
+	ShowDisplay();
+}
+
+void normalLv_board(pGameData ap_data)
+{
+	Clear();
+	for (int y = 0; y < NORMAL_Y_COUNT; y++) {
+		for (int x = 0; x < NORMAL_X_COUNT; x++) {
+			Rectangle(x * NORMAL_GRID_SIZE, y * NORMAL_GRID_SIZE, (x + 1) * NORMAL_GRID_SIZE, (y + 1) * NORMAL_GRID_SIZE, RGB(0, 100, 200), RGB(0, 0, 128));
+		}
+	}
+
+	for (int y = 0; y < NORMAL_Y_COUNT; y++) {
+		for (int x = 0; x < NORMAL_X_COUNT; x++) {
+			if (ap_data->state_hard[y][x] == nothing_open)
+				Rectangle(x * NORMAL_GRID_SIZE, y * NORMAL_GRID_SIZE, (x + 1) * NORMAL_GRID_SIZE, (y + 1) * NORMAL_GRID_SIZE, RGB(200, 100, 0), RGB(128, 0, 0));
+			else if (ap_data->state_hard[y][x] == flag)
+				Ellipse(x * NORMAL_GRID_SIZE, y * NORMAL_GRID_SIZE, (x + 1) * NORMAL_GRID_SIZE, (y + 1) * NORMAL_GRID_SIZE, RGB(200, 100, 0), RGB(128, 0, 0));
+			else if (ap_data->state_hard[y][x] == questionMark)
+				Ellipse(x * NORMAL_GRID_SIZE, y * NORMAL_GRID_SIZE, (x + 1) * NORMAL_GRID_SIZE, (y + 1) * NORMAL_GRID_SIZE, WHITE, BLACK);
+		}
+	}
+
+	ShowDisplay();
+}
+
+void hardLv_board(pGameData ap_data)
+{
+	Clear();
+	for (int y = 0; y < HARD_Y_COUNT; y++) {
+		for (int x = 0; x < HARD_X_COUNT; x++) {
+			Rectangle(x * HARD_GRID_SIZE, y * HARD_GRID_SIZE, (x + 1) * HARD_GRID_SIZE, (y + 1) * HARD_GRID_SIZE, RGB(0, 100, 200), RGB(0, 0, 128));
+		}
+	}
+
+	for (int y = 0; y < HARD_Y_COUNT; y++) {
+		for (int x = 0; x < HARD_X_COUNT; x++) {
+			if (ap_data->state_hard[y][x] == nothing_open)
+				Rectangle(x * HARD_GRID_SIZE, y * HARD_GRID_SIZE, (x + 1) * HARD_GRID_SIZE, (y + 1) * HARD_GRID_SIZE, RGB(200, 100, 0), RGB(128, 0, 0));
+			else if (ap_data->state_hard[y][x] == flag)
+				Ellipse(x * HARD_GRID_SIZE, y * HARD_GRID_SIZE, (x + 1) * HARD_GRID_SIZE, (y + 1) * HARD_GRID_SIZE, RGB(200, 100, 0), RGB(128, 0, 0));
+			else if (ap_data->state_hard[y][x] == questionMark)
+				Ellipse(x * HARD_GRID_SIZE, y * HARD_GRID_SIZE, (x + 1) * HARD_GRID_SIZE, (y + 1) * HARD_GRID_SIZE, WHITE, BLACK);
+		}
+	}
+
+	ShowDisplay();
+}
